b989f492e2f14c5f8ac15f67d37b86ab7aa2335a
VeronikaLapteva <v.a.strelnikova@mail.ru>
2023-08-23 14:41:27 +0300

    updated settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,18 +1,17 @@
 # flake8: noqa
 import os
 from pathlib import Path
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY',default=' ')
 
 DEBUG = True
 
-ALLOWED_HOSTS = '51.250.103.160','127.0.0.1','localhost','kittygramlbee.chickenkiller.com'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
